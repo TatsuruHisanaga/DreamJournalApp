ef71635023ca2cc5d059fb3faaffc82955ec2e84
Tatsuru Hisanaga <tatsu72x@gmail.com>
2023-10-05 23:20:19 +0900

    style: 🎨 Highlight top 3 ranks with specific background colors

--- components/RankingList.js
+++ components/RankingList.js
@@ -12,12 +12,25 @@ const RankingList = ({ data, userData }) => {
     }
   }
 
+  const rankStyle = (rank) => {
+    switch(rank) {
+      case 0: // 1位
+        return styles.firstPlace;
+      case 1: // 2位
+        return styles.secondPlace;
+      case 2: // 3位
+        return styles.thirdPlace;
+      default:
+        return null;
+    }
+  }
+
   return (
     <FlatList 
       data={data}
       keyExtractor={item => item.id.toString()}
       renderItem={({ item, index }) => (
-        <View style={[styles.listItem, switchBgColor(item.id)]}>
+        <View style={[styles.listItem, switchBgColor(item.id), rankStyle(index)]}>
           <Text style={styles.rank}>{index + 1}</Text>
           {/* アバター画像を追加したい */}
           <Text style={styles.name}>{item.name}</Text>
@@ -50,6 +63,15 @@ const styles = StyleSheet.create({
   },
   score: {
     fontSize: 16,
+  },
+  firstPlace: {
+    backgroundColor: 'gold',
+  },
+  secondPlace: {
+    backgroundColor: 'silver',
+  },
+  thirdPlace: {
+    backgroundColor: '#cd7f32', // bronze color
   },
 });
 
