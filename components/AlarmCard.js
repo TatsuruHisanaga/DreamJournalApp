4f2bd6577b750fc7df1d7098269ad6d95aa26ab4
Tatsuru Hisanaga <tatsu72x@gmail.com>
2023-10-07 15:33:28 +0900

    feat(AlarmCard): ✨ Revamp UI with badges and switch, add activation toggle
    ### コミット内容
    react-native-elements の Badge と Switch コンポーネントが追加されています。
    新たに isActive と toggleAlarm のプロパティが追加されています。
    スタイルが大幅に変更され、新しいスタイルシートが追加されています。

--- components/AlarmCard.js
+++ components/AlarmCard.js
@@ -1,14 +1,66 @@
 import React from 'react';
-import { Text } from 'react-native';
-import { Card } from 'react-native-elements';
-
-export default function AlarmCard({ time, days }) {
+import { Text, View, StyleSheet } from 'react-native';
+import { Card, Badge, Switch } from 'react-native-elements';
+export default function AlarmCard({ time, days, isActive, toggleAlarm }) {
   return (
-    <Card containerStyle={{ marginTop: 40 }}>
-      <Card.Title>アラーム設定</Card.Title>
+    <Card containerStyle={styles.cardContainer}>
+      <Card.Title style={styles.cardTitle}>アラーム設定</Card.Title>
       <Card.Divider />
-      <Text style={{ marginBottom: 10 }}>時刻: {time.toString()}</Text>
-      <Text style={{ marginBottom: 10 }}>曜日: {JSON.stringify(days)}</Text>
+      <View style={styles.timeContainer}>
+        <Text style={styles.label}>時刻: </Text>
+        <Text style={styles.time}>{time.toLocaleTimeString()}</Text>
+      </View>
+      <View style={styles.daysContainer}>
+        {Object.keys(days).map((day) => (
+          days[day] && <Badge value={day} badgeStyle={styles.dayBadge} textStyle={styles.dayText} key={day} />
+        ))}
+      </View>
+      <View style={styles.switchContainer}>
+        <Switch
+          value={isActive}
+          onValueChange={toggleAlarm}
+        />
+      </View>
     </Card>
   );
 }
+
+const styles = StyleSheet.create({
+  cardContainer: {
+    marginTop: 40,
+    borderRadius: 10,
+    width: '85%',
+    alignSelf: 'center',
+    alignContent: 'strech',
+  },
+  cardTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  timeContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  daysContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  label: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  time: {
+    fontSize: 16,
+  },
+  dayBadge: {
+    backgroundColor: '#6200ea',
+    marginHorizontal: 5,
+  },
+  dayText: {
+    color: '#fff',
+  },
+  switchContainer: {
+
+  },
+});
