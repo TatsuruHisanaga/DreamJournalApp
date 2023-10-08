2f3b1a1a72102340cfb0d66ed46082e727affef7
Tetsu-is <hap30305@kwansei.ac.jp>
2023-10-08 19:47:27 +0900

    move alarm list amd setButton to alarmScreen

--- screens/AlarmScreen.js
+++ screens/AlarmScreen.js
@@ -1,11 +1,62 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from "react";
+import { Button } from "react-native-elements";
+import AlarmSettingOverlay from "../components/AlarmSettingOverlay";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import AlarmCard from "../components/AlarmCard";
 
 export default function AlarmScreen() {
+  const [alarms, setAlarms] = useState([]);
+  const [visible, setVisible] = useState(false);
+  const [time, setTime] = useState(new Date());
+  const [days, setDays] = useState({
+    Mon: false,
+    Tue: false,
+    Wed: false,
+    Thu: false,
+    Fri: false,
+    Sat: false,
+    Sun: false,
+  });
+
+  const toggleOverlay = () => {
+    setVisible(!visible);
+  };
+
+  const addAlarm = () => {
+    setAlarms([...alarms, { time, days }]);
+    toggleOverlay();
+  };
+
+  const onTimeChange = (e, selectedTime) => {
+    setTime(selectedTime || time);
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>アラーム一覧</Text>
       {/* ここにアラームの一覧を表示するコードを追加 */}
+      <FlatList
+        data={alarms}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => (
+          <AlarmCard time={item.time} days={item.days} />
+        )}
+      />
+      <Button
+        title="アラーム設定"
+        onPress={toggleOverlay}
+        containerStyle={{ marginBottom: 30 }}
+      />
+
+      <AlarmSettingOverlay
+        visible={visible}
+        toggleOverlay={toggleOverlay}
+        addAlarm={addAlarm}
+        time={time}
+        onTimeChange={onTimeChange}
+        days={days}
+        setDays={setDays}
+      />
     </View>
   );
 }
@@ -13,12 +64,12 @@ export default function AlarmScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5F5F5',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5F5F5",
   },
   title: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
 });
--- screens/HomeView.js
+++ screens/HomeView.js
@@ -8,43 +8,11 @@ import DreamJournalCard from '../components/DreamJournalCard';
 import RingingAlarm from '../components/RingingAlarm';
 
 export default function HomeView() {
-  const [alarms, setAlarms] = useState([]);
-  const [visible, setVisible] = useState(false);
-  const [time, setTime] = useState(new Date());
-  const [days, setDays] = useState({
-    Mon: false,
-    Tue: false,
-    Wed: false,
-    Thu: false,
-    Fri: false,
-    Sat: false,
-    Sun: false,
-  });
-  const [isAlarmRinging, setIsAlarmRinging] = useState(false); //trueの時にカウントアップが始まる
-
-  const toggleOverlay = () => {
-    setVisible(!visible);
-  };
-
-  const addAlarm = () => {
-    setAlarms([...alarms, { time, days }]);
-    toggleOverlay();
-  };
-
-  const onTimeChange = (e, selectedTime) => {
-    setTime(selectedTime || time);
-  };
+  const [isAlarmRinging, setIsAlarmRinging] = useState(true); //trueの時にカウントアップが始まる
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       {/* <RankingView /> */}
-      <FlatList
-        data={alarms}
-        keyExtractor={(item, index) => index.toString()}
-        renderItem={({ item }) => (
-          <AlarmCard time={item.time} days={item.days} />
-          )}
-      />
       <DreamJournalCard
         entry={{
           name: 'テストユーザー',
@@ -52,20 +20,6 @@ export default function HomeView() {
           title: 'テストの夢のタイトル',
           details: 'テストの夢の詳細',
         }}
-      />
-      <Button
-        title="アラーム設定"
-        onPress={toggleOverlay}
-        containerStyle={{ marginBottom: 30 }}
-      />
-      <AlarmSettingOverlay
-        visible={visible}
-        toggleOverlay={toggleOverlay}
-        addAlarm={addAlarm}
-        time={time}
-        onTimeChange={onTimeChange}
-        days={days}
-        setDays={setDays}
       />
       <RingingAlarm
         isAlarmRinging={isAlarmRinging}
