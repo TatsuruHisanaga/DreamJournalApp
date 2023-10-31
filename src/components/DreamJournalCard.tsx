4fcdf1b9e2d3b181afa002d5b53915b6a1df00eb
Tatsuru Hisanaga <tatsu72x@gmail.com>
2023-10-31 11:39:56 +0900

    feat: #1 ✨ Extend DreamJournalCard with onPress and onUpdate callbacks

--- src/components/DreamJournalCard.tsx
+++ src/components/DreamJournalCard.tsx
@@ -5,10 +5,12 @@ import { Entry } from '../types//EntryTypes';
 
 interface DreamJournalCardProps {
   entry: Entry;
+  onPress: (entry: Entry) => void;
+  onUpdate: (entry: Entry) => void;
 }
 
-const DreamJournalCard: React.FC<DreamJournalCardProps> = ({ entry }) => {
-  return <DreamJournalCommonCard entry={entry} />;
+const DreamJournalCard: React.FC<DreamJournalCardProps> = ({ entry, onPress, onUpdate }) => {
+  return <DreamJournalCommonCard entry={entry} onPress={() => onPress(entry)} onUpdate={() => onUpdate(entry)} />;
 };
 
 export default DreamJournalCard;
