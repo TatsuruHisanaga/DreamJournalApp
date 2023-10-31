969fca47e4c304a1b07e9e06a4f4d139de791a8a
Tatsuru Hisanaga <tatsu72x@gmail.com>
2023-10-31 12:00:17 +0900

    feat: #1 ✨ Enhance DreamJournalScreen with Entry Selection and Update
    - Add `selectedEntry` state to keep track of the currently selected journal entry
    - Implement `handleSelect` to set the selected entry
    - Implement `handleUpdate` to update an existing entry
    - Modify `handleSave` to handle both new entries and updates to existing entries
    - Pass `onSelect`, `onUpdate`, and `onPress` handlers to DreamJournalContainer
    - Pass `selectedEntry` to DreamJournalModal for pre-filling form fields
    
    Note: This allows users to select, update, and view details of journal entries more interactively.

--- src/screens/DreamJournalScreen.tsx
+++ src/screens/DreamJournalScreen.tsx
@@ -8,17 +8,39 @@ import { Entry } from '../types//EntryTypes';
 
 const DreamJournalScreen: React.FC = () => {
   const [entries, setEntries] = useState<Entry[]>([]);
+  const [selectedEntry, setSelectedEntry] = useState<Entry | null>(null); // 選択したエントリの状態を追加
 
   const handleSave = async (entry: Entry) => {
-    const newEntries = [...entries, entry];
-    setEntries(newEntries);
-    await AsyncStorage.setItem('dreamJournal', JSON.stringify(newEntries));
+    if (selectedEntry) {
+      const updatedEntries = entries.map((e) => e.id === selectedEntry.id ? entry : e);
+      setEntries(updatedEntries);
+      setSelectedEntry(null);
+    } else {
+      const newEntries = [...entries, entry];
+      setEntries(newEntries);
+    }
+    await AsyncStorage.setItem('dreamJournal', JSON.stringify(entries));
+  };
+
+  const handleSelect = (entry: Entry) => {
+    setSelectedEntry(entry);
+  };
+
+  const handleUpdate = (updatedEntry: Entry) => {
+    const updatedEntries = entries.map((entry) =>
+      entry.id === updatedEntry.id ? updatedEntry : entry
+    );
+    setEntries(updatedEntries);
+  };
+
+  const handlePress = (entry: Entry) => {
+    console.log(entry);
   };
 
   return (
     <View style={styles.container}>
-      <DreamJournalContainer entries={entries} setEntries={setEntries} />
-      <DreamJournalModal handleSave={handleSave} />
+      <DreamJournalContainer entries={entries} setEntries={setEntries} onSelect={handleSelect} onUpdate={handleUpdate} onPress={handlePress} />
+      <DreamJournalModal handleSave={handleSave} selectedEntry={selectedEntry} />
     </View>
   );
 };
